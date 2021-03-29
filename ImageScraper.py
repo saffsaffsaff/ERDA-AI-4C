0a5fb855aa742b5e473ffaa858a8f711f0047bbf
Savier <savieraa@hotmail.com>
2021-03-29 21:18:16 +0200

    Update ImageScraper.py

--- ImageScraper.py
+++ ImageScraper.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-# = 'https://www.jetphotos.com/showphotos.php?aircraft=all&airline=all&country-location=location%253BAmsterdam%2520Schiphol%2520Airport%2520-%2520EHAM&photographer-group=all&category=all&keywords-type=all&keywords-contain=3&keywords=&photo-year=all&genre=all&search-type=Advanced&sort-order=0'
-
 def aeroplanedatabase(url, folder):
     try:
         os.mkdir(os.path.join(os.getcwd(), folder))
@@ -19,15 +17,13 @@ def aeroplanedatabase(url, folder):
     for image in images[4:]:
         #name = alt text
         name = image['alt']
-        #image link = source link from css
-        link = image['src']
-        #make link usable
-        link1 = link.replace('//', 'http://', 1)
+        #image link = source link from css + make link usable
+        link = image['src'].replace('//', 'http://', 1)
         #make name usable
         print(name.split('----'))
         #save images as jpeg in assigned folder
         with open(name.replace('/', '') + '.JPG', 'wb') as f:
-            im = requests.get(link1)
+            im = requests.get(link)
             f.write(im.content)
 
 
